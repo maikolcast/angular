f2b89a14e315040eca88fb9d8b338469860b20a0
maikolcast <maikol.castano1991@gmail.com>
2023-11-19 19:03:40 -0500

    limpieza

--- peliculas/src/app/models/pelicula.model.ts
+++
@@ -1,8 +0,0 @@
-export class Pelicula {
-  constructor(
-    public id: number,
-    public titulo: string,
-    public portada: string // URL de la imagen de la portada
-  ) // Agrega aquí más propiedades según los datos de tu API
-  {}
-}
